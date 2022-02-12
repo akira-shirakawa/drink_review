9a500df537b366e73dd2cdbf2999d339508f8732
akira-shirakawa <akun32310901@yahoo.co.jp>
2022-02-13 03:50:03 +0900

    chnaged

--- database/migrations/2022_02_08_000148_likes.php
+++ database/migrations/2022_02_08_000148_likes.php
@@ -13,14 +13,14 @@ class Likes extends Migration
      */
     public function up()
     {
-        // Schema::create('post_user', function (Blueprint $table) {
-        //     $table->bigIncrements('id');
-        //     $table->bigInteger('user_id');
-        //     $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-        //     $table->bigInteger('post_id');
-        //     $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
-        //     $table->timestamps();
-        // });
+        Schema::create('post_user', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->bigInteger('post_id');
+            $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
+            $table->timestamps();
+        });
     }
 
     /**
